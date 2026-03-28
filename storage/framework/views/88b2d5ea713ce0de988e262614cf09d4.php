5e2adbc1990ea1ce4d066845b0e9bb6b7728e886
sansayan01 <msayan9733@gmail.com>
2026-03-29 01:50:09 +0530

    Restored individual permissions button for primary SAs

--- storage/framework/views/88b2d5ea713ce0de988e262614cf09d4.php
+++ storage/framework/views/88b2d5ea713ce0de988e262614cf09d4.php
@@ -170,14 +170,4 @@ $effectiveUser = \App\Models\User::getEffectiveUser();
         </li>
     <?php endif; ?>
 
-    <li>
-        <a href="<?php echo e(route('profile.edit')); ?>"
-            class="flex items-center space-x-3 px-4 py-3 rounded-lg text-bodydark hover:text-white hover:bg-secondary transition font-medium <?php echo e(request()->routeIs('profile.*') ? 'bg-accent text-white shadow-lg' : ''); ?>">
-            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
-            </svg>
-            <span>Account Settings</span>
-        </a>
-    </li>
 </ul><?php /**PATH C:\xampp\htdocs\HF\resources\views/layouts/partials/sidebar_nav.blade.php ENDPATH**/ ?>
